4cd8006f2c53b1b79bdfa2bab93799e61dbb4b3e
Ovakoma <ideleovercomer18@gmail.com>
2022-08-24 15:25:57 -0700

    Updated 0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+last = 122
+while last > 96:
+    check = 0
+    if (last % 2 != 0):
+        last -= 32
+        check = 1
+    print(f"{chr(last)}", end="")
+    if (check):
+        last += 32
+    last -= 1

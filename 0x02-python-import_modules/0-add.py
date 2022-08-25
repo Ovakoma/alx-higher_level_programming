c5f49f9a99625f3a49150aca94f84d9af94032dc
Ovakoma <ideleovercomer18@gmail.com>
2022-08-25 08:53:09 -0700

    Updated 0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from function import add
+from add_0 import add
 
 def main():
     a = 1
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 def main():
-    num_args = len(argv) - 1
-    if (num_args < 1):
-        print("{} arguments.".format(num_args))
-    elif (num_args == 1):
-        print("{} argument:\n{}: {}".format(num_args, num_args, argv[1]))
-    elif (num_args > 1):
-        for i in range(1, len(argv))
-        print("{} arguments:\n{}: {}".format(num_args, num_args, argv[i]))
+    n_arg = len(argv) - 1
+    if (n_arg < 1):
+        print("{} arguments.".format(n_arg))
+    elif (n_arg == 1):
+        print("{} argument:\n{}: {}".format(n_arg, n_arg, argv[1]))
+    elif (n_arg > 1):
+        print("{} arguments:".format(n_arg))
+        for i in range(1, len(argv)):
+            print('{}: {}'.format(i, argv[i]))
 
 if __name__ == "__main__":
     main()

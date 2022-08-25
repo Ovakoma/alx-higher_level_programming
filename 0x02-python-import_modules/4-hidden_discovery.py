fda850f1876f43c87a16206daecf9604b0faee9b
Ovakoma <ideleovercomer18@gmail.com>
2022-08-25 15:06:57 -0700

    Updated 0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
 def main():
-    for name in dir(hidden_4):
+    for name in dir(hide):
         if not (name[0] == "_" and name[1] == "_"):
             print(name)
 
 if __name__ == "__main__":
+    import hidden_4 as hide
     main()

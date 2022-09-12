4ff1019584710a517963f6dd9bf8ff9141549d69
Ovakoma <ideleovercomer18@gmail.com>
2022-09-12 14:28:09 -0700

    Updated 0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     #function that divides 2 integers and prints the result
     try:
         result = a / b
-    except ZeroDivisionError:
+    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))

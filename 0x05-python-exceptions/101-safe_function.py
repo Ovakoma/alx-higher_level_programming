1c4efd70e2916ebc1206366b19ba940e577f2ade
Ovakoma <ideleovercomer18@gmail.com>
2022-09-12 14:56:43 -0700

    Updated 0x05-python-exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,3 +6,5 @@ def safe_function(fct, *args):
     except (ZeroDivisionError, ValueError, TypeError, IndexError) as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return None
+    finally:
+        return result

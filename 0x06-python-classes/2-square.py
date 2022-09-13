5cd11ca854020a66abf23b5002ba470a4200c89f
Ovakoma <ideleovercomer18@gmail.com>
2022-09-13 15:10:09 -0700

    Updated 0x06-python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,16 +3,16 @@
 
 
 class Square:
-    """ class defines sqaure """
+    """ class defines square """
 
     def __init__(self, size=0):
-        """ Initialize new square
+        """ Initialize new square.
 
         Args:
-            size(int): size of square 
+            size(int): size of square.
         """
         if not isinstance(size, int):
-            raise TypeError('size must be integer')
+            raise TypeError("size must be integer")
         elif size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         self.__size = size

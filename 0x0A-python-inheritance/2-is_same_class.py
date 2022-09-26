503366095a62e6f4213db2f5f72521f5e0f3a7de
Ovakoma <ideleovercomer18@gmail.com>
2022-09-26 20:37:20 +0100

    first commit

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module initialises is_same_class(...) function."""
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an instance
+    of the specified class; otherwise False."""
+    return True if isinstance(obj, a_class) else False
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""module contains an empty class."""
+
+class BaseGeometry:
+    """initialises an empty class."""
+    pass

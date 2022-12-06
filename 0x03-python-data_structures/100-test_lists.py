094127840c1f3f6812e1882284920c8f3410fa74
Ovakoma <ideleovercomer18@gmail.com>
2022-12-06 02:25:21 +0100

    Updated 0x03-python-data_structures

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -2,20 +2,20 @@ import ctypes
 
 lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
+l_b = ['hello', 'World']
 lib.print_python_list_info(l)
 del l[1]
 lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
+l_b = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
 lib.print_python_list_info(l)
-l = []
+l_b = []
 lib.print_python_list_info(l)
-l.append(0)
+l_b.append(0)
 lib.print_python_list_info(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
+l_b.append(1)
+l_b.append(2)
+l_b.append(3)
+l_b.append(4)
 lib.print_python_list_info(l)
-l.pop()
+l_b.pop()
 lib.print_python_list_info(l)

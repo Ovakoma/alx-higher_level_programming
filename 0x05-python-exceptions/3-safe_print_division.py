72e4eeeb349cfdf181a2661bbb22a00381a463d5
Ovakoma <ideleovercomer18@gmail.com>
2022-12-19 23:59:35 +0100

    Updated 0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
         print("Inside result: {:.1f}".format(result))
-    except Execption as ex:
+    except Execption:
         result = None
         print("Inside result: {}".format(result))
     finally:

671a3031c13f6c27fbe608a4b664cb936352e63a
Ovakoma <ideleovercomer18@gmail.com>
2023-01-02 23:15:29 +0100

    Updated 0x08-python-more_classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
-''' 
+'''
     column first element, row second element
 '''
+
+
 def reject(board):
     for col_A in board:
         for col_B in board:
-            if not col_A is col_B:
+            if col_A not in col_B:
                 if col_A[0] == col_B[0]:
                     return True
                 if col_A[1] == col_B[1]:
